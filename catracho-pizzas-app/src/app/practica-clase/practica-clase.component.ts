21819bbf643a37767fc7bcd6113e81ef6bf26f4e
Nachtwander <131110427+Nachtwander@users.noreply.github.com>
2024-08-10 18:17:13 -0600

    IsOpen Alert y AlertController
    Aprendiendo el uso de IsOpen y ControllerAlert

--- catracho-pizzas-app/src/app/practica-clase/practica-clase.component.ts
+++ catracho-pizzas-app/src/app/practica-clase/practica-clase.component.ts
@@ -4,6 +4,7 @@ import {
   IonContent,
   IonItem,
   IonButton,
+  AlertController,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -14,24 +15,100 @@ import {
   imports: [IonAlert, IonContent, IonItem, IonButton],
 })
 export class PracticaClaseComponent implements OnInit {
-  constructor() {}
-
   ngOnInit() {}
 
   BotonesAlertByID = [
     //primer boton de la alerta
+    //Todos los botones cuentan con 4 propiedades: text, role, cssClass y handler
     {
       text: 'Cancelar',
       role: 'cancel',
       cssClass: 'secondary',
+      handler: () => {
+        //aqui en el handler de debe colocar toda la logica a usar con el boton implementado.
+        //este es un metodo VOID
+        console.log('console.log dentro del handler del boton Cancelar');
+      },
     },
     //segundo boton de la alerta
     {
       text: 'Ok',
+      handler: () => this.mensajeAlert(),
+    },
+    //Se pueden seguir colocando mas botones segun la necesidad.
+  ];
+
+  //Funcion que se utiliza de ejemplo en el handler del boton ok.
+  mensajeAlert() {
+    console.log('se llamo a una funcion externa.');
+  }
+
+  //---------------------------------------------------------------------------------------------------------
+  // Varible que se utiliza para determinar si se debe o no mostrar la Alerta IsOpen
+  isOpen: boolean = false;
+
+  //Funcion que se usa con Event Binding (click) para cambiar el valor de la variable isOpen
+  openAlert(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  //misma logica de botones de Inline Alert para los IsOpen
+  BotonesAlertIsOpen = [
+    //primer boton de la alerta
+    //Todos los botones cuentan con 4 propiedades: text, role, cssClass y handler
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      cssClass: 'secondary',
       handler: () => {
-        //aqui en el handler de deber colocar toda la logica a usar con el boton implementado.
-        console.log('Boton ok');
+        //aqui en el handler de debe colocar toda la logica a usar con el boton implementado.
+        //este es un metodo VOID
+        console.log('console.log dentro del handler del boton Cancelar');
       },
     },
+    //segundo boton de la alerta
+    {
+      text: 'Ok',
+      handler: () => this.mensajeAlert(),
+    },
+    //Se pueden seguir colocando mas botones segun la necesidad.
+  ];
+
+  //---------------------------------------------------------------------------------------------------------
+
+  //Injectamos la clase AlertController que construya las alertas con la variable alertController
+  constructor(private alertController: AlertController) {}
+
+  async alertControllerFunction(): Promise<void> {
+    //creamos una alerta mandando a llamar la clase alerController y el metodo .create
+    //este metodo es una promesa, asi que debe esperar a ejencutarse para crear una alerta
+    const alert: HTMLIonAlertElement = await this.alertController.create({
+      header: 'Titulo',
+      subHeader: 'Subtitulo',
+      message: 'Mensaje a insertar en la alerta.',
+      buttons: this.BotonesAlertController,
+    });
+    await alert.present();
+  }
+
+  BotonesAlertController = [
+    //primer boton de la alerta
+    //Todos los botones cuentan con 4 propiedades: text, role, cssClass y handler
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      cssClass: 'secondary',
+      handler: () => {
+        //aqui en el handler de debe colocar toda la logica a usar con el boton implementado.
+        //este es un metodo VOID
+        console.log('console.log dentro del handler del boton Cancelar');
+      },
+    },
+    //segundo boton de la alerta
+    {
+      text: 'Ok',
+      handler: () => this.mensajeAlert(),
+    },
+    //Se pueden seguir colocando mas botones segun la necesidad.
   ];
 }
